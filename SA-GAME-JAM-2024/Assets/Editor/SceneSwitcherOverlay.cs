b98abd2cdd9fa6dbf5a4a3f99ca1b8d6b6c3d0ee
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-10-18 19:03:33 +0200

    Initial setup P1

---
+++ SA-GAME-JAM-2024/Assets/Editor/SceneSwitcherOverlay.cs
@@ -0,0 +1,51 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+[InitializeOnLoad]
+public class SceneSwitcherOverlay
+{
+    static SceneSwitcherOverlay()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        Handles.BeginGUI();
+
+        GUILayout.BeginArea(new Rect(10, 10, 200, 1000));
+        GUILayout.Label("Scene Switcher", EditorStyles.boldLabel);
+
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string sceneName = Path.GetFileNameWithoutExtension(scenePath);
+
+            if (GUILayout.Button(sceneName))
+            {
+                LoadSceneByIndex(i);
+            }
+        }
+
+        GUILayout.EndArea();
+        Handles.EndGUI();
+    }
+
+    private static void LoadSceneByIndex(int sceneIndex)
+    {
+        if (sceneIndex >= 0 && sceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(sceneIndex);
+            Debug.Log("Scene loaded: " + SceneUtility.GetScenePathByBuildIndex(sceneIndex));
+        }
+        else
+        {
+            Debug.LogWarning("Invalid scene index. Ensure the scene is added to Build Settings.");
+        }
+    }
+}
+#endif
